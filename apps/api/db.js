5e5d7ce66a14949555b48ba17e4d4a17b04f1f69
serenella angelilli <serenellaangelilli@MacBook-Pro-di-serenella.local>
2026-05-04 14:27:10 +0200

    fix database connection for render

--- apps/api/db.js
+++ apps/api/db.js
@@ -1,10 +1,23 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  user: "serenellaangelilli",
-  host: "localhost",
-  database: "marketplace",
-  port: 5432,
-});
+const isProduction = process.env.NODE_ENV === "production";
+
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: isProduction
+          ? {
+              rejectUnauthorized: false,
+            }
+          : false,
+      }
+    : {
+        user: "serenellaangelilli",
+        host: "localhost",
+        database: "marketplace",
+        port: 5432,
+      }
+);
 
 module.exports = pool;
